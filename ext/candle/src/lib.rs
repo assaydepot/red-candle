c37a6b867bf84293ec2a32f19ed6e33aba22ef53
kojix2 <2xijok@gmail.com>
2023-09-02 05:31:31 +0000

    Add methods

--- ext/candle/src/lib.rs
+++ ext/candle/src/lib.rs
@@ -1,26 +1,48 @@
-use std::cell::RefCell;
 use magnus::{function, method, prelude::*, Error, Ruby};
 
 //use candle_core::{DType, Device, Tensor, WithDType};
 
 #[magnus::wrap(class = "Candle::Tensor", free_immediately, size)]
-struct Tensor(RefCell<candle_core::Tensor>);
+struct Tensor(candle_core::Tensor);
+
+#[magnus::wrap(class = "Candle::DType", free_immediately, size)]
+struct DType(candle_core::DType);
 
 impl Tensor {
     fn new(array: Vec<f32>) -> Self {
         use candle_core::Device::Cpu;
-        Self(RefCell::new(candle_core::Tensor::new(array.as_slice(), &Cpu).unwrap()))
+        Self(candle_core::Tensor::new(array.as_slice(), &Cpu).unwrap())
     }
     fn shape(&self) -> Vec<usize> {
-        self.0.borrow().dims().to_vec()
+        self.0.dims().to_vec()
+    }
+    fn stride(&self) -> Vec<usize> {
+        self.0.stride().to_vec()
+    }
+    fn dtype(&self) -> DType {
+        DType(self.0.dtype())
     }
     fn rank(&self) -> usize {
-        self.0.borrow().rank()
+        self.0.rank()
+    }
+    fn __repr__(&self) -> String {
+    format!("{}", self.0)
+    }
+
+    fn __str__(&self) -> String {
+        self.__repr__()
     }
 }
 
-#[magnus::wrap(class = "Candle::DType", free_immediately, size)]
-struct DType(candle_core::DType);
+impl DType {
+    fn __repr__(&self) -> String {
+        format!("{:?}", self.0)
+    }
+
+    fn __str__(&self) -> String {
+        self.__repr__()
+    }
+}
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
@@ -29,6 +51,9 @@ fn init(ruby: &Ruby) -> Result<(), Error> {
     let class2 = module.define_class("DType", Ruby::class_object(ruby))?;
     class1.define_singleton_method("new", function!(Tensor::new, 1))?;
     class1.define_method("shape", method!(Tensor::shape, 0))?;
+    class1.define_method("stride", method!(Tensor::stride, 0))?;
+    class1.define_method("dtype", method!(Tensor::dtype, 0))?;
     class1.define_method("rank", method!(Tensor::rank, 0))?;
+    class1.define_method("to_s", method!(Tensor::__str__, 0))?;
     Ok(())
 }
