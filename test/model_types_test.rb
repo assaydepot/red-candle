3cdee542f16da39e0dc4d87a20c5c882b985b61c
Chris Petersen <chris@petersen.io>
2025-05-02 09:22:56 -0700

    Adding some logging when a model fails and trying to use googles bert model

--- test/model_types_test.rb
+++ test/model_types_test.rb
@@ -3,7 +3,7 @@ require_relative "test_helper"
 class ModelTypesTest < Minitest::Test
   MODEL_TYPES = {
     Candle::ModelType::JINA_BERT => "jinaai/jina-embeddings-v2-base-en",
-    Candle::ModelType::STANDARD_BERT => "bert-base-uncased",
+    Candle::ModelType::STANDARD_BERT => "google-bert/bert-base-uncased",
     Candle::ModelType::MINILM => "sentence-transformers/all-MiniLM-L6-v2",
     Candle::ModelType::SENTIMENT => "distilbert-base-uncased-finetuned-sst-2-english",
     Candle::ModelType::LLAMA => "meta-llama/Llama-2-7b"
@@ -15,7 +15,7 @@ class ModelTypesTest < Minitest::Test
       embedding_size: 768
     },
     Candle::ModelType::STANDARD_BERT => {
-      model_path: "bert-base-uncased",
+      model_path: "google-bert/bert-base-uncased",
       embedding_size: 768
     },
     Candle::ModelType::MINILM => {
@@ -75,14 +75,22 @@ class ModelTypesTest < Minitest::Test
 
   def test_safetensor_model_types_initialize
     SAFETENSOR_MODELS.each do |model_type, info|
-      model = Candle::Model.new(
-        model_path: info[:model_path],
-        tokenizer_path: info[:model_path],
-        model_type: model_type,
-        device: nil,
-        embedding_size: info[:embedding_size]
-      )
-      assert model, "Model should be initialized for #{model_type}"
+      begin
+        model = Candle::Model.new(
+          model_path: info[:model_path],
+          tokenizer_path: info[:model_path],
+          model_type: model_type,
+          device: nil,
+          embedding_size: info[:embedding_size]
+        )
+        assert model, "Model should be initialized for #{model_type}"
+      rescue => e
+        puts e.message
+        puts e.backtrace.join("\n")
+        puts "Initializing model for [#{model_type}]"
+        puts info.inspect
+        raise e
+      end
     end
   end
 
