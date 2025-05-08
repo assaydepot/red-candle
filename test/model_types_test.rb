3918576623a73c452b13834bd233f83ecab2cfde
Chris Petersen <chris@petersen.io>
2025-05-08 12:03:38 -0700

    Simplifying the tests

--- test/model_types_test.rb
+++ test/model_types_test.rb
@@ -21,62 +21,69 @@ class ModelTypesTest < Minitest::Test
     # Add more safetensors models as needed
   }
 
-  def test_model_types_initialize
-    # Test all model types with their default Hugging Face repos
-    SAFETENSOR_MODELS.each do |model_type, model_options|
-      model_options.each do |model_option|
-        model_path = model_option[:model_path]
-        embedding_size = model_option[:embedding_size]
-        puts ">>>>>>>>>>>>> model_path #{model_path} model_type #{model_type}"
-
-        if model_type == Candle::ModelType::STANDARD_BERT
-          # These official models do not provide safetensors, should error helpfully
-          assert_raises RuntimeError, /model\.safetensors not found|Only safetensors models are supported/ do
-            Candle::Model.new(
-              model_path: model_path,
-              tokenizer_path: model_path,
-              model_type: model_type,
-              device: nil,
-              embedding_size: embedding_size
-            )
-          end
-        else
-          # Should succeed for models with safetensors
-          model = Candle::Model.new(
-            model_path: model_path,
-            tokenizer_path: model_path,
-            model_type: model_type,
-            device: nil,
-            embedding_size: embedding_size
-          )
-          assert model, "Model should be initialized for #{model_type}"
-        end
-      end
-    end
+  # Helper to initialize a model and assert correctness
+  private def initialize_model_for_type(model_type, model_option)
+    model_path = model_option[:model_path]
+    embedding_size = model_option[:embedding_size]
+    model = Candle::Model.new(
+      model_path: model_path,
+      tokenizer_path: model_path,
+      model_type: model_type,
+      device: nil,
+      embedding_size: embedding_size
+    )
+    assert model, "Model should be initialized for #{model_type}"
   end
 
-  def test_safetensor_model_types_initialize
-    SAFETENSOR_MODELS.each do |model_type, model_options|
-      model_options.each do |model_option|
-        model_path = model_option[:model_path]
-        embedding_size = model_option[:embedding_size]
-        begin
-          model = Candle::Model.new(
-            model_path: model_path,
-            tokenizer_path: model_path,
-            model_type: model_type,
-            device: nil,
-            embedding_size: embedding_size
-          )
-          assert model, "Model should be initialized for #{model_type}"
-        rescue => e
-          puts e.message
-          puts e.backtrace.join("\n")
-          puts "Initializing model for [#{model_type}]"
-          puts model_option.inspect
-          raise e
-        end
-      end
-    end
+  # Individual tests for each model type (explicitly written)
+
+  def test_initialize_jina_bert_0
+    model_type = Candle::ModelType::JINA_BERT
+    model_option = { model_path: "jinaai/jina-embeddings-v2-base-en", embedding_size: 768 }
+    initialize_model_for_type(model_type, model_option)
   end
+
+  def test_safetensor_initialize_jina_bert_0
+    model_type = Candle::ModelType::JINA_BERT
+    model_option = { model_path: "jinaai/jina-embeddings-v2-base-en", embedding_size: 768 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+  def test_initialize_standard_bert_0
+    model_type = Candle::ModelType::STANDARD_BERT
+    model_option = { model_path: "scientistcom/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext", embedding_size: 768 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+  def test_safetensor_initialize_standard_bert_0
+    model_type = Candle::ModelType::STANDARD_BERT
+    model_option = { model_path: "scientistcom/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext", embedding_size: 768 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+  def test_initialize_minilm_0
+    model_type = Candle::ModelType::MINILM
+    model_option = { model_path: "sentence-transformers/all-MiniLM-L6-v2", embedding_size: 384 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+  def test_safetensor_initialize_minilm_0
+    model_type = Candle::ModelType::MINILM
+    model_option = { model_path: "sentence-transformers/all-MiniLM-L6-v2", embedding_size: 384 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+  def test_initialize_distilbert_0
+    model_type = Candle::ModelType::DISTILBERT
+    model_option = { model_path: "scientistcom/distilbert-base-uncased-finetuned-sst-2-english", embedding_size: 768 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+  def test_safetensor_initialize_distilbert_0
+    model_type = Candle::ModelType::DISTILBERT
+    model_option = { model_path: "scientistcom/distilbert-base-uncased-finetuned-sst-2-english", embedding_size: 768 }
+    initialize_model_for_type(model_type, model_option)
+  end
+
+
 end
