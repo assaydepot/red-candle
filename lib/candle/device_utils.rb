2cb17b23344475607fd7b41d92f97e3046dd9945
Chris Petersen <chris@petersen.io>
2025-07-09 07:32:04 -0700

    Get rid of DeviceUtils.create_with_best_device

--- lib/candle/device_utils.rb
+++ lib/candle/device_utils.rb
@@ -16,23 +16,5 @@ module Candle
         end
       end
     end
-    
-    # Create a model with automatic device selection
-    # This is a convenience method that automatically picks the best device
-    def self.create_with_best_device(model_class, **options)
-      device = options.delete(:device) || best_device
-      
-      case model_class.name
-      when "Candle::EmbeddingModel"
-        model_class.new(**options, device: device)
-      when "Candle::Reranker"
-        model_class.new(**options, device: device)
-      when "Candle::LLM"
-        model_id = options.delete(:model_id) || options.delete(:model_path)
-        model_class.from_pretrained(model_id, device, **options)
-      else
-        raise "Unknown model class: #{model_class}"
-      end
-    end
   end
 end
