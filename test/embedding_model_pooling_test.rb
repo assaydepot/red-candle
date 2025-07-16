9e46195664683bbdda9cbbd7482edd9692b0c14e
Christopher Petersen <chris@petersen.io>
2025-07-16 08:49:37 -0700

    Use element-wise pooling assertions

--- test/embedding_model_pooling_test.rb
+++ test/embedding_model_pooling_test.rb
@@ -11,7 +11,9 @@ class EmbeddingModelPoolingTest < Minitest::Test
     pooled_ruby = @model.pool_embedding(@embeddings)
     # Manually pool by averaging over sequence dimension (1)
     pooled_manual = @embeddings.mean(1)
-    assert_in_delta pooled_ruby.first.to_a[0], pooled_manual.first.to_a[0], 1e-5
+    pooled_ruby.first.to_a.each_index do |i|
+      assert_in_delta pooled_ruby.first.to_a[i], pooled_manual.first.to_a[i], 1e-5
+    end
   end
 
   def test_pool_and_normalize_embedding_matches_manual
@@ -19,7 +21,9 @@ class EmbeddingModelPoolingTest < Minitest::Test
     # Manually pool and then normalize
     pooled_manual = @embeddings.mean(1)
     norm = pooled_manual / Math.sqrt(pooled_manual.sqr.sum(1).values.first)
-    assert_in_delta pooled_norm_ruby.first.to_a[0], norm.first.to_a[0], 1e-5
+    pooled_norm_ruby.first.to_a.each_index do |i|
+      assert_in_delta pooled_norm_ruby.first.to_a[i], norm.first.to_a[i], 1e-5
+    end
   end
 
   def test_pool_cls_embedding
@@ -27,7 +31,9 @@ class EmbeddingModelPoolingTest < Minitest::Test
     # Manually extract CLS token (index 0)
     # For shape [batch, seq, hidden], get first token of first batch
     cls_manual = @embeddings.get(0).get(0)
-    assert_in_delta cls_ruby.first.to_a[0], cls_manual.to_a[0], 1e-5
+    cls_ruby.first.to_a.each_index do |i|
+      assert_in_delta cls_ruby.first.to_a[i], cls_manual.to_a[i], 1e-5
+    end
   end
 
   def test_embedding_calls_correct_pooling
