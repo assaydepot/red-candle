26634fc84261b49a4d3ef3547b6046313fe37b53
Chris Petersen <chris@petersen.io>
2025-08-09 14:16:32 -0700

    Fixing a yard documentation error

--- lib/candle/device_utils.rb
+++ lib/candle/device_utils.rb
@@ -1,6 +1,6 @@
 module Candle
   module DeviceUtils
-    # @deprecated Use {Device.best} instead
+    # @deprecated Use {Candle::Device.best} instead
     # Get the best available device (Metal > CUDA > CPU)
     def self.best_device
       warn "[DEPRECATION] `DeviceUtils.best_device` is deprecated. Please use `Device.best` instead."
